f0c0ac2ec6fe94c146b7e1ddc7718d2fd3da2319
fquiachon <fquiachon26@gmail.com>
2020-12-12 12:54:49 +0800

    [PE-2] Create API that could allow user to CRUD a stock tickers

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, String, Integer
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from os import path
+from utils.ticker import global_tickers
 
 
 app = Flask(__name__)
@@ -14,6 +15,63 @@ app.config['JWT_SECRET_KEY'] = 'super-secret' # test key only
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 jwt = JWTManager(app)
+
+
+@app.route('/global/tickers', methods=['POST'])
+@jwt_required
+def add_global_tickers():
+    existing_tickers = []
+    added_tickers = []
+
+    if request.is_json:
+        tickers = request.json['tickers'].split(',')
+    else:
+        tickers = request.form['tickers'].split(',')
+
+    for ticker in tickers:
+        if ticker in global_tickers:
+            existing_tickers.append(ticker)
+        else:
+            added_tickers.append(ticker)
+            global_tickers.append(ticker)
+
+    message = {
+        'status': 201,
+        'Message': f'Ticker Successfully added, {added_tickers}',
+    }
+
+    if len(existing_tickers) == 0:
+            message['Error'] = []
+    else:
+        message['Error'] = f'Tickers already exist, {existing_tickers}'
+
+    if len(added_tickers) == 0:
+        message['status'] = 409
+        message['Message'] = []
+
+    return jsonify(message), message['status']
+
+
+@app.route('/global/tickers', methods=['GET'])
+@jwt_required
+def get_global_tickers():
+    return jsonify({'tickers' : global_tickers}), 200
+
+
+@app.route('/global/tickers', methods=['DELETE'])
+@jwt_required
+def del_global_tickers():
+    deleted_tickers = []
+    if request.is_json:
+        tickers = request.json['tickers'].split(',')
+    else:
+        tickers = request.form['tickers'].split(',')
+
+    for ticker in tickers:
+        if ticker in global_tickers:
+            removed_item = global_tickers.pop(global_tickers.index(ticker))
+            deleted_tickers.append(removed_item)
+    return jsonify({'Successfully deleted tickers': deleted_tickers}), 200
 
 
 @app.route('/')
